6ace62e9bf1dea730051076409b9970569e3a951
Aaron Hinni <aaron@hinnispace.com>
2015-01-03 11:24:43 -0600

    color code the choices

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,17 @@ exports.question = function (req, res) {
   Questions.lookupByOffset(req.params.index, function (err, question) {
     if (err) console.log(err);
     else console.log(util.inspect(question));
+
+    var colors = ["primary","success","info","warning","danger"];
+
     res.render('question', {
-      question: question
+      question: question,
+      coloredChoices: _.map(question.choices, function (value, index) {
+        return {
+          color: colors[index % colors.length],
+          value: value
+        };
+      })
     });
   });
-}
+};
